e02e3f1bb7b416c8a214e36c58566cc4fdce2681
M4rtin052 <262116@student.pwr.edu.pl>
2021-11-03 00:45:09 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,8 +17,19 @@ public:
     ~MainWindow();
 
 private slots:
-
+    // Pythagoras
+    void on_firstBox_valueChanged(double value);
+    void on_thirdBox_valueChanged(double value);
+    void on_fifthBox_valueChanged(double value);
+    // Trigonometry
+    void on_Degrees_valueChanged(double value);
+    // R/I/U
+    void on_intensity_valueChanged(double value);
+    void on_tension_valueChanged(double value);
+    void on_tension2_valueChanged(double value);
 private:
     Ui::MainWindow *ui;
 };
+
+
 #endif // MAINWINDOW_H
